ff61ef588c74498360868c2a7a3624e6f84116d9
Андрей Хвостов <HomeMacbook@MacBook-Pro-Andrej.local>
2021-06-19 09:52:26 +0300

    hw v2.0

--- sender.py
+++ sender.py
@@ -1,8 +1,9 @@
 import asyncio
 import sqlite3
 from email.message import EmailMessage
-
 import aiosmtplib
+
+message = EmailMessage()
 
 
 def get_users_data():
@@ -13,16 +14,25 @@ def get_users_data():
         return data
 
 
-async def sender(data):
-    message = EmailMessage()
-    message["From"] = "root@localhost"
-    for person in data:
-        message["To"] = person[2]
-        message["Subject"] = f'"Уважаемый {person[0]} {person[1]}! ' \
-                             f'Спасибо, что пользуетесь нашим сервисом объявлений."'
-        await aiosmtplib.send(message, hostname="127.0.0.1", port=25)
+async def sender(messenger, data):
+    for name, lastname, email in data:
+        messenger["From"] = "root@localhost"
+        messenger["To"] = email
+        messenger["Subject"] = f'"Уважаемый {name} {lastname}! ' \
+                               f'Спасибо, что пользуетесь нашим сервисом объявлений."'
+        await aiosmtplib.send(
+            messenger,
+            hostname="smtp.yandex.ru",
+            port=465,
+            username="skitol@yandex.ru",
+            password="qxgymlbzrzaypdlt"
+        )
+
+
+async def main():
+    await sender(message, get_users_data())
 
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    asyncio.run(sender(get_users_data()))
+    asyncio.run(main())
+
